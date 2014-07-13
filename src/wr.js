a333139ae03abe0cf9e8fe89dfd57179190caf47
Hirotaka Nakajima <hiro@skyblue.me.uk>
2014-07-14 00:06:01 +0900

    Remove ws:// and wss:// protocol

--- src/wr.js
+++ src/wr.js
@@ -13,12 +13,12 @@ function onBeforeRequest(details) {
   var tmphost = tmpuri.host();
   var finalUri = '';
   tmphost.replace(ipAddrRegex,function(str, p1, p2, p3, p4, offset, s){
-  		finalUri = tmpuri.host(p1+"."+p2+"."+p3+"."+p4+v4_suffix).toString();
-	});
- if('' != finalUri) {
- 	console.log(finalUri);
- 	return {redirectUrl: finalUri};
- }
+    finalUri = tmpuri.host(p1+"."+p2+"."+p3+"."+p4+v4_suffix).toString();
+  });
+  if('' != finalUri) {
+    console.log(finalUri);
+    return {redirectUrl: finalUri};
+  }
 };
 
-wr.onBeforeRequest.addListener(onBeforeRequest, {urls: ["https://*/*", "http://*/*", "ws://*/*", "wss://*/*", "ftp://*/*"]}, ["blocking"]);
+wr.onBeforeRequest.addListener(onBeforeRequest, {urls: ["https://*/*", "http://*/*", "ftp://*/*"]}, ["blocking"]);
